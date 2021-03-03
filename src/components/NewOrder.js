95eb7e2c3f3d35576fa8e60d4a79d796517cf71e
Jake Barber <jabot999@gmail.com>
2021-03-03 16:18:36 -0500

    Added tooltip on submit order to ensure user has enough funds deposited to exchange

--- src/components/NewOrder.js
+++ src/components/NewOrder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import {Tabs, Tab} from 'react-bootstrap'
+import {Tabs, Tab, OverlayTrigger, Tooltip} from 'react-bootstrap'
 import Spinner from './Spinner'
 import {
     exchangeSelector,
@@ -37,45 +37,63 @@ const showForm = (props) => {
 
     //console.log("MakeBuyOrder: ", exchange)
 
+    //Sufficient funds must be deposeted to the exchange from the appropriate account to ensure the order can complete, otherwise, a contract exception will be shown in MetaMask when attempting to fill an order
+    const renderTooltip = (props) => (
+        <Tooltip id="button-tooltip" {...props}>
+          Please be sure to have enough funds deposited to the exchange to make sure the order can complete.
+        </Tooltip>
+    );
+
     //using bootstrap tabs, imported above
     return(
         <Tabs defaultActiveKey="buy" className='bg-dark text-white'>
             <Tab eventKey="buy" title="Buy" className="bg-dark">
 
-            <form classname="row" onSubmit={(event) =>{
-                event.preventDefault()
-                makeBuyOrder(dispatch, exchange, token, web3, buyOrder, account)
-            }}>
-                <div className="form-group small">
-                    <label>Buy Amount (BB)</label>
-                    <div className="input-group">
-                        <input
-                            type="text"
-                            placeholder="Buy Amount (BB)"
-                            onChange={(e) => dispatch(buyOrderAmountChanged(e.target.value))/*dispatch change event to redux*/}
-                            className="form-control form-control-sm bg-dark text-white"
-                            required
-                        />
-                    </div>                    
-                </div>
-                <div className="form-group small">
-                    <label>Buy Price (ETH)</label>
-                    <div className="input-group">
-                        <input
-                            type="text"
-                            placeholder="Buy Price (ETH)"
-                            onChange={(e) => dispatch(buyOrderPriceChanged(e.target.value))/*dispatch change event to redux*/}
-                            className="form-control form-control-sm bg-dark text-white"
-                            required
-                        />
-                    </div>                    
-                </div>
-                <button type="submit" className="btn btn-primary btn-block btn-sm">SUBMIT BUY ORDER</button>
-                {showBuyTotal ? <small>Total: {buyOrder.amount * buyOrder.price /**calculate total */} ETH</small> : null}
-                
-            </form>
+                <form classname="row" onSubmit={(event) =>{
+                    event.preventDefault()
+                    makeBuyOrder(dispatch, exchange, token, web3, buyOrder, account)
+                }}>
+                    <div className="form-group small">
+                        <label>Buy Amount (BB)</label>
+                        <div className="input-group">
+                            <input
+                                type="text"
+                                placeholder="Buy Amount (BB)"
+                                onChange={(e) => dispatch(buyOrderAmountChanged(e.target.value))/*dispatch change event to redux*/}
+                                className="form-control form-control-sm bg-dark text-white"
+                                required
+                            />
+                        </div>                    
+                    </div>
+                    <div className="form-group small">
+                        <label>Buy Price (ETH)</label>
+                        <div className="input-group">
+                            <input
+                                type="text"
+                                placeholder="Buy Price (ETH)"
+                                onChange={(e) => dispatch(buyOrderPriceChanged(e.target.value))/*dispatch change event to redux*/}
+                                className="form-control form-control-sm bg-dark text-white"
+                                required
+                            />
+                        </div>                    
+                    </div>
+                    <OverlayTrigger
+                        placement="right"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip}
+                    >
+                        <button 
+                        type="submit" 
+                        className="btn btn-primary btn-block btn-sm"
+                        >
+                            SUBMIT BUY ORDER
+                        </button>
+                    </OverlayTrigger>
+                    {showBuyTotal ? <small>Total: {buyOrder.amount * buyOrder.price /**calculate total */} ETH</small> : null}
+                    
+                </form>
              
-
+           
 
 
             </Tab>
@@ -108,7 +126,18 @@ const showForm = (props) => {
                         />
                     </div>
                     </div>
-                    <button type="submit" className="btn btn-primary btn-sm btn-block">SUBMIT SELL ORDER</button>
+                    <OverlayTrigger
+                        placement="right"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={renderTooltip}
+                    >
+                        <button 
+                        type="submit" 
+                        className="btn btn-primary btn-block btn-sm"
+                        >
+                            SUBMIT SELL ORDER
+                        </button>
+                    </OverlayTrigger>
                     { showSellTotal ? <small>Total: {sellOrder.amount * sellOrder.price} ETH</small> : null }
                 </form>
             </Tab>
